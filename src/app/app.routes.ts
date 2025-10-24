b4027c2968f7db1a0616dd365b3ac05136160027
Mykola846 <stryzhakov356@gmail.com>
2025-10-24 11:12:30 +0300

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home';
 import { ProductsComponent } from './pages/products/products';
 import { Login } from './pages/login/login';
 import { ProductDetails } from './pages/product-details/product-details';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'login', component: Login },
   { path: 'product/:id', component: ProductDetails },
-  { path: '**', redirectTo: '' }
+  { path: '404', component: NotFound },
+  { path: '**', component: NotFound }
 ];
